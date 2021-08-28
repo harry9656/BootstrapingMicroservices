0d7c35dc4157bebd4d95dcf9cc23c26ab399a763
Harpal Singh <singharpal9656@gmail.com>
2021-08-28 04:57:06 +0200

    Added Video endpoint

--- Chapter2/index.js
+++ Chapter2/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/video', (req, res) => {
+    const PATH = "./video/SampleVideo_1280x720_1mb.mp4";
+    fs.stat(PATH, (err, stats) => {
+        if(err){
+            console.log("An error occured");
+            res.sendStatus(500);
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+
+        fs.createReadStream(PATH).pipe(res);
+    })
 });
 
 app.listen(PORT, () => {
